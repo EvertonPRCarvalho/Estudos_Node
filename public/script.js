26e5db3abc8e22950066509d8a2b302b1f75374f
Everton Carvalho <49604482+EvertonPRCarvalho@users.noreply.github.com>
2023-05-19 22:42:00 -0300

    create-account button hide

--- public/script.js
+++ public/script.js
@@ -5,6 +5,40 @@ function toggleMenu() {
     const navList = document.getElementById('nav');
     navList.classList.toggle('active');
 }
+
+var element1 = document.getElementById("form-login-step-one");
+var element2 = document.getElementById("form-login-step-tow");
+var element3 = document.getElementById("form-login-step-three");
+var element4 = document.getElementById("btn-next-step");
+function nextStepCreateAccount(){
+    if (element1.style.display != "none"){
+        element1.style.display = "none";    
+        element2.style.display = "block";   
+        console.log(element1.style.display);
+        console.log(element2.style.display);
+        console.log(element3.style.display);
+        console.log("estou dentro do IF");
+    } else if (element1.style.display == "none" && element2.style.display == "none" )
+    {
+        element2.style.display = "block";
+        element1.style.display = "none";
+        element3.style.display = "none";
+        console.log("estou dentro do ELSE IF");
+        console.log(element1.style.display);
+        console.log(element2.style.display);
+        console.log(element3.style.display);
+    }else {
+        element1.style.display = "none";
+        element2.style.display = "none";
+        element4.style.display = "none";
+        element3.style.display = "block";
+
+        console.log("estou dentro do ELSE");
+        console.log(element1.style.display);
+        console.log(element2.style.display);
+        console.log(element3.style.display);
+    }
+}
 /*
 btnMobile.addEventListener('click', toggleMenu);
 
