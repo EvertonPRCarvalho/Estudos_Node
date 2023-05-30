10c75cec711da4a3ed1a6aaa366a898a69940db7
Everton Carvalho <49604482+EvertonPRCarvalho@users.noreply.github.com>
2023-05-29 21:32:09 -0300

    categoria_v0

---
+++ controller/ViewsController.js
@@ -0,0 +1,68 @@
+const createAccount = async (req, res) => {
+    try {
+        return res.render("create-account");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const home = async (req, res) => {
+    try {
+        return res.render("home");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const itensDoados = async (req, res) => {
+    try {
+        return res.render("itens-doados");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const login = async (req, res) => {
+    try {
+        return res.render("login");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const ongs = async (req, res) => {
+    try {
+        return res.render("ongs");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const doarItem = async (req, res) => {
+    try {
+        return res.render("doar-item");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const index = async (req, res) => {
+    try {
+        return res.render("index");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+const categoria = async (req, res) => {
+    try {
+        return res.render("categoria");
+    } catch (err) {
+        res.status(500).send({error: err.message});
+    }
+};
+
+
+module.exports = {
+    createAccount,
+    home,
+    itensDoados,
+    login,
+    ongs,
+    doarItem,
+    index,
+    categoria
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,15 @@
 const routes = require("express").Router();
-const FirstPageController = require("../controller/FirstPageController");
-const Login = require("../controller/LoginController");
-const ONGs = require("../controller/OngsController");
-const CreateAccount = require("../controller/CreateAccountController");
-const DoaItem = require("../controller/DoarItemController");
-const ItensDoados = require("../controller/ItensDoadosController");
-const Home = require("../controller/HomeController");
-const Teste = require("../controller/TesteController")
+const Routes = require("../controller/ViewsController");
 
+routes.get("/", Routes.index);
+routes.get("/login", Routes.login);
+routes.get("/ongs", Routes.ongs);
+routes.get("/create-account", Routes.createAccount);
+routes.get("/doar-item", Routes.doarItem);
+routes.get("/itens-doados", Routes.itensDoados);
+routes.get("/categoria", Routes.categoria);
+routes.get("/home", Routes.home);
+routes.get("/index", Routes.index);
 
-routes.get("/", FirstPageController.getFirstPage);
-routes.get("/login", Login.login);
-routes.get("/ongs", ONGs.ongs);
-routes.get("/create-account", CreateAccount.createAccount);
-routes.get("/doar-item", DoaItem.getDoarItem);
-routes.get("/itens-doados", ItensDoados.getAllItensDoados);
-routes.get("/home", Home.getAllItens);
-routes.get("/teste", Teste.teste);
 
 module.exports = routes;
